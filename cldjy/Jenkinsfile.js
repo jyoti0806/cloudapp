f934ade30e9f5b5bfdfe4dbc35a8dfe0832f0726
jyoti patil <jtpatil43@gmail.com>
2019-08-09 09:28:29 +0000

    tstdone

--- cldjy/Jenkinsfile.js
+++ cldjy/Jenkinsfile.js
@@ -1,11 +1,11 @@
 @Library('piper-library-os') _
 
 node(){
-  //stage('Prepare')   {
-  //    deleteDir()
-  //    checkout scm
-  //    setupCommonPipelineEnvironment script:this
-  //}
+  stage('Prepare')   {
+      deleteDir()
+      checkout scm
+      setupCommonPipelineEnvironment script:this
+  }
 
   stage('Build')   {
       mtaBuild script:this
